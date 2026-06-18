f111bc097a091e49fea05725d7829632fb6eb23c
cirila432 <wandoski.priscila@escola.pr.gov.br>
2026-06-18 13:07:24 -0300

    ççç

--- JS main.js
+++ JS main.js
@@ -3,3 +3,5 @@ cont numeroSenha=document.querySelector("."parametro-senha__texto"");
 numeroSenha.textContent = 5;
 
 cost botoes- document.querySelector(".parametro-senha__botao")
+
+console.log(botoes);
