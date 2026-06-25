8420b88f848ab75c9d714eae1af475413c9c3ae3
cirila432 <wandoski.priscila@escola.pr.gov.br>
2026-06-25 08:21:36 -0300

    gg

--- JS main.js
+++ JS main.js
@@ -1,7 +1,7 @@
-const numeroSenha=document.querySelector("."parametro-senha__texto"");
+const numeroSenha=document.querySelector('. parametro-senha__texto');
 
 numeroSenha.textContent = 5;
 
-const botoes = document.querySelector(".parametro-senha__botao")
+const botoes = document.querySelector('.parametro-senha__botao');
 
 console.log(botoes);
